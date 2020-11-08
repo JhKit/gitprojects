1855eafefc1463b10c6fc52fbee492dd45c3b32c
JunHao1996 <852348184@qq.com>
2020-11-08 10:50:50 +0800

    commit

--- netcoreapp/NetCoreApp/FuncUnit/TestFuncDemo.cs
+++ netcoreapp/NetCoreApp/FuncUnit/TestFuncDemo.cs
@@ -1,12 +1,17 @@
-using system;
+using System;
+
 namespace TestFuncDemo
 {
     public class TestFunc
     {
         static Func<T,R> Memoize<T,R>(Func<T,R> func) where T :IComparable
         {
-
-            
+            Dictionary<T,R> cache = new  Dictionary<T,R>();
+            return arg =>{
+                if(cache.ContainsKey(arg))
+                    return cache[arg];
+                return (cache[arg]=func(arg));
+            };            
         }        
     }    
 }
